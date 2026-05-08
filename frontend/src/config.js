3938e41fd1fc14fd9201fb3a04606bce569515af
unknown <diveshk960@gmail.com>
2026-05-08 20:31:21 +0530

    fixed api url

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,2 +1,3 @@
-const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5000";
+const API_URL =
+  import.meta.env.VITE_API_URL || "https://portfolio-1-lyk3.onrender.com";
 export default API_URL;
