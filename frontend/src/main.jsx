bd0138916a88b30205295af06563a007f267c28d
unknown <diveshk960@gmail.com>
2026-06-02 19:30:51 +0530

    fix main.jsx import order

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,1 +1,13 @@
-a
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import { startKeepAlive } from "./utils/keepAlive";
+
+startKeepAlive();
+
+createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
