38a6d2f632adf4ffb4769a05861189f1e116f010
unknown <diveshk960@gmail.com>
2026-06-02 19:27:25 +0530

    fix keepAlive interval 14min to 5min

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,1 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import {startKeepAlive} from "./utils/keepAlive";
-
-startKeepAlive();
-import App from "./App";
-import "./index.css";
-
-createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+a
--- frontend/src/utils/keepAlive.js
+++ frontend/src/utils/keepAlive.js
@@ -6,11 +6,12 @@ export function startKeepAlive() {
   if (intervalId) return;
 
   const ping = () => {
-    fetch(`${API_URL}/api/profile`)
+    fetch(`${API_URL}/ping`)
       .then(() => console.log("[KeepAlive] Backend awake ✅"))
       .catch(() => console.warn("[KeepAlive] Backend waking up..."));
   };
 
   ping();
-  intervalId = setInterval(ping, 14 * 60 * 1000);
+  intervalId = setInterval(ping, 5 * 60 * 1000); 
 }
+    
